15271b3a955571e35820400938b84c9f104e0791
Navnit Anuth <nanuth@qaworks.com>
2017-04-09 22:41:21 +0100

    Initial Hello World of Alexa

---
+++ Alexa/Controllers/AlexaController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http;
+
+namespace Alexa.Controllers
+{
+	public class AlexaController : ApiController
+	{
+		[HttpPost, Route("api/alexa/demo")]
+		public dynamic Pluralsight(dynamic request)
+		{
+			return new
+			{
+				version = "1.0",
+				sessionAttributes = new { },
+				response = new
+				{
+					outputSpeech = new
+					{
+						type = "PlainText",
+						text = "Hello World"
+					},
+					card = new
+					{
+						type = "Simple",
+						title = "Horoscope",
+						content = "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."
+					},
+					shouldEndSession = true
+				}
+			};
+		}
+
+	}
+}
